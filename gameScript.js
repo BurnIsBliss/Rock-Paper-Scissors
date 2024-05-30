7bfca10ff8cec6c7194a6a75626b99c57aeb9455
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-05-30 06:29:55 +0530

    Stage 2 and 3: getHumanChoice and playRound functions addedd

--- gameScript.js
+++ gameScript.js
@@ -1,11 +1,46 @@
 function getComputerChoice (){
     let x = Math.random();
-    console.log(x);
     if (x >=0 && x < (1/3)) return "Rock";
     else if (x >= (1/3) && x < (2/3)) return "Paper";
     else return "Scissors";
-    
 }
 
+
+function getHumanChoice (){
+    let input = prompt("Please enter your choice from these: 'Rock', 'Paper', or 'Scissors'");
+    switch (input.toLowerCase()){
+        case "rock":
+            return "Rock";
+         
+        case "paper":
+            return "Paper";
+        
+        case "scissors":
+            return "Scissors";
+        
+        default:
+            alert ("Enter a valid choice");
+    }
+}
+
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == computerChoice) console.log("It's a tie!!!!!");
+    else if ((humanChoice == "Rock" && computerChoice == "Paper") || (humanChoice == "Paper" && compChoice == "Scissors") || (humanChoice == "Scissors" && compChoice == "Rock")) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore += 1;
+    }
+    else  console.log(`You Win! ${humanChoice} beats ${computerChoice}.`); humanScore += 1;
+}
+
+
+
+let humanScore, computerScore;
+humanScore = computerScore = 0;
+
+
 let compChoice = getComputerChoice();
-console.log(compChoice);
+let humanChoice = getHumanChoice();
+
+playRound(humanChoice, compChoice);
+// console.log(Math.floor(Math.random() * 3));
