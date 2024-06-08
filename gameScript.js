08ebe7ef29a0bce871b147e478d12ee7ccfd0cb3
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-06-08 11:14:20 +0530

    Introduced buttons and implemented their click action

--- gameScript.js
+++ gameScript.js
@@ -6,26 +6,26 @@ function getComputerChoice (){
 }
 
 
-function getHumanChoice (){
-    let input = prompt("Please enter your choice from these: 'Rock', 'Paper', or 'Scissors'.");
-    if (input){
+// function getHumanChoice (){
+//     let input = prompt("Please enter your choice from these: 'Rock', 'Paper', or 'Scissors'.");
+//     if (input){
 
-        switch (input.toLowerCase()){
-            case "rock":
-                return "Rock";
+//         switch (input.toLowerCase()){
+//             case "rock":
+//                 return "Rock";
             
-            case "paper":
-                return "Paper";
+//             case "paper":
+//                 return "Paper";
             
-            case "scissors":
-                return "Scissors";
+//             case "scissors":
+//                 return "Scissors";
             
-            default:
-                alert ("Enter a valid choice");
-                return 0;
-        }
-    }
-}
+//             default:
+//                 alert ("Enter a valid choice");
+//                 return 0;
+//         }
+//     }
+// }
 
 
 function playRound(humanChoice, computerChoice){
@@ -42,25 +42,30 @@ function playRound(humanChoice, computerChoice){
 }
 
 
-function playGame() {
+function playGame(buttonValue) {
     let compChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
+    let humanChoice = buttonValue;
 
     // console.log(`The main function ${humanChoice} and comp: ${compChoice}`);
 
-    if (humanChoice) playRound(humanChoice, compChoice);
+    playRound(humanChoice, compChoice);
     
 }
 
 let humanScore, computerScore;
 humanScore = computerScore = 0;
 
-for (let i = 0; i < 5; i++){
-    console.log(`Round ${i+1}`);
-    playGame();
-}
+const button_set = document.querySelectorAll("button");
+// console.log(button_set);
 
+// for (let i = 0; i < 5; i++){
+//     console.log(`Round ${i+1}`);
+//     playGame();
+// }
 
-console.log(`The final score is Human: ${humanScore} |||||| Computer: ${computerScore}`);
+if ((humanScore == 5) || (computerScore == 5))
+    console.log(`The final score is Human: ${humanScore} |||||| Computer: ${computerScore}`);
+else
+    button_set.forEach((buttons) => {buttons.addEventListener("click", () => playGame(buttons.textContent))});  
 
 // console.log(Math.floor(Math.random() * 3));
